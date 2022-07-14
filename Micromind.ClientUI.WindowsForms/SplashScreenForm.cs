908524b4596d7568cbe93e4ddb9fb15f1133908e
eng Abubakr Mahdi <engabubakrmahdi@hotmail.com>
2022-07-14 08:54:15 +0200

    Changed Splash Screen.
    Changed splash screen image to smart+ image.

--- Micromind.ClientUI.WindowsForms/SplashScreenForm.cs
+++ Micromind.ClientUI.WindowsForms/SplashScreenForm.cs
@@ -121,34 +121,38 @@ namespace Micromind.ClientUI.WindowsForms
             // marqueeProgressBarControl1
             // 
             this.marqueeProgressBarControl1.EditValue = 0;
-            this.marqueeProgressBarControl1.Location = new System.Drawing.Point(23, 231);
+            this.marqueeProgressBarControl1.Location = new System.Drawing.Point(31, 284);
+            this.marqueeProgressBarControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.marqueeProgressBarControl1.Name = "marqueeProgressBarControl1";
-            this.marqueeProgressBarControl1.Size = new System.Drawing.Size(404, 12);
+            this.marqueeProgressBarControl1.Size = new System.Drawing.Size(539, 15);
             this.marqueeProgressBarControl1.TabIndex = 5;
             this.marqueeProgressBarControl1.Visible = false;
             // 
             // labelControl1
             // 
             this.labelControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.labelControl1.Location = new System.Drawing.Point(23, 286);
+            this.labelControl1.Location = new System.Drawing.Point(31, 352);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(115, 13);
+            this.labelControl1.Size = new System.Drawing.Size(135, 16);
             this.labelControl1.TabIndex = 6;
             this.labelControl1.Text = "Copyright © 1998-2011";
             this.labelControl1.Visible = false;
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(23, 206);
+            this.labelControl2.Location = new System.Drawing.Point(31, 254);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(50, 13);
+            this.labelControl2.Size = new System.Drawing.Size(57, 16);
             this.labelControl2.TabIndex = 7;
             this.labelControl2.Text = "Starting...";
             this.labelControl2.Visible = false;
             // 
             // pictureEdit2
             // 
-            this.pictureEdit2.Location = new System.Drawing.Point(12, 12);
+            this.pictureEdit2.Location = new System.Drawing.Point(16, 15);
+            this.pictureEdit2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureEdit2.Name = "pictureEdit2";
             // 
             // 
@@ -158,14 +162,15 @@ namespace Micromind.ClientUI.WindowsForms
             this.pictureEdit2.Properties.Appearance.Options.UseBackColor = true;
             this.pictureEdit2.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
             this.pictureEdit2.Properties.ShowMenu = false;
-            this.pictureEdit2.Size = new System.Drawing.Size(426, 180);
+            this.pictureEdit2.Size = new System.Drawing.Size(568, 222);
             this.pictureEdit2.TabIndex = 9;
             this.pictureEdit2.Visible = false;
             // 
             // pictureEdit1
             // 
             this.pictureEdit1.EditValue = ((object)(resources.GetObject("pictureEdit1.EditValue")));
-            this.pictureEdit1.Location = new System.Drawing.Point(278, 268);
+            this.pictureEdit1.Location = new System.Drawing.Point(371, 330);
+            this.pictureEdit1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureEdit1.Name = "pictureEdit1";
             // 
             // 
@@ -175,7 +180,7 @@ namespace Micromind.ClientUI.WindowsForms
             this.pictureEdit1.Properties.Appearance.Options.UseBackColor = true;
             this.pictureEdit1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
             this.pictureEdit1.Properties.ShowMenu = false;
-            this.pictureEdit1.Size = new System.Drawing.Size(155, 48);
+            this.pictureEdit1.Size = new System.Drawing.Size(207, 59);
             this.pictureEdit1.TabIndex = 8;
             this.pictureEdit1.Visible = false;
             // 
@@ -183,9 +188,10 @@ namespace Micromind.ClientUI.WindowsForms
             // 
             this.labelTrial.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
             this.labelTrial.Appearance.Options.UseFont = true;
-            this.labelTrial.Location = new System.Drawing.Point(52, 221);
+            this.labelTrial.Location = new System.Drawing.Point(69, 272);
+            this.labelTrial.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.labelTrial.Name = "labelTrial";
-            this.labelTrial.Size = new System.Drawing.Size(104, 13);
+            this.labelTrial.Size = new System.Drawing.Size(128, 17);
             this.labelTrial.TabIndex = 16;
             this.labelTrial.Text = "Evaluation Version";
             this.labelTrial.Visible = false;
@@ -196,18 +202,20 @@ namespace Micromind.ClientUI.WindowsForms
             this.labelEdition.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(65)))), ((int)(((byte)(113)))));
             this.labelEdition.Appearance.Options.UseFont = true;
             this.labelEdition.Appearance.Options.UseForeColor = true;
-            this.labelEdition.Location = new System.Drawing.Point(52, 200);
+            this.labelEdition.Location = new System.Drawing.Point(69, 246);
+            this.labelEdition.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.labelEdition.Name = "labelEdition";
-            this.labelEdition.Size = new System.Drawing.Size(0, 15);
+            this.labelEdition.Size = new System.Drawing.Size(0, 18);
             this.labelEdition.TabIndex = 17;
             this.labelEdition.Visible = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.pictureBox1.Location = new System.Drawing.Point(2, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(35, 25);
+            this.pictureBox1.Size = new System.Drawing.Size(47, 31);
             this.pictureBox1.TabIndex = 18;
             this.pictureBox1.TabStop = false;
             // 
@@ -217,9 +225,10 @@ namespace Micromind.ClientUI.WindowsForms
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold);
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(195, 197);
+            this.label4.Location = new System.Drawing.Point(260, 242);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(257, 20);
+            this.label4.Size = new System.Drawing.Size(321, 25);
             this.label4.TabIndex = 136;
             this.label4.Text = "Business Managment System";
             // 
@@ -229,9 +238,10 @@ namespace Micromind.ClientUI.WindowsForms
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(173, 161);
+            this.label3.Location = new System.Drawing.Point(231, 198);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(283, 31);
+            this.label3.Size = new System.Drawing.Size(347, 39);
             this.label3.TabIndex = 135;
             this.label3.Text = "Starasoft © StarERP";
             // 
@@ -241,17 +251,18 @@ namespace Micromind.ClientUI.WindowsForms
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(496, 358);
+            this.label1.Location = new System.Drawing.Point(661, 441);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.Size = new System.Drawing.Size(216, 17);
             this.label1.TabIndex = 137;
             this.label1.Text = "Copyright Starasoft © 2019-2020";
             // 
             // SplashScreenForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(670, 359);
+            this.ClientSize = new System.Drawing.Size(893, 432);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -263,6 +274,7 @@ namespace Micromind.ClientUI.WindowsForms
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.marqueeProgressBarControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SplashScreenForm";
             this.ShowMode = DevExpress.XtraSplashScreen.ShowMode.Image;
             this.SplashImage = ((System.Drawing.Image)(resources.GetObject("$this.SplashImage")));
